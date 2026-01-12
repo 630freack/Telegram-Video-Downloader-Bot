b8e676892980b0522d99f51af221688d86c3d59a
630freack <sollojtuteam@vk.com>
2026-01-12 11:55:43 +0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,4 @@
+﻿class Test:
+    __private = 1
+
+print([x for x in dir(Test) if 'private' in x])
---
+++ test_updater.py
@@ -0,0 +1,10 @@
+﻿from telegram.ext import Updater
+from telegram import Bot
+import asyncio
+
+async def test():
+    bot = Bot('test')
+    updater = Updater(bot=bot, update_queue=asyncio.Queue())
+    print('Updater created successfully')
+
+asyncio.run(test())
